27a49086427ce7cc5fcfa6413da084d16af9fe91
AcerLaptop <acerLaptop@DESKTOP-O9LVU8P>
2022-11-28 21:24:44 +0300

    entity configiration

---
+++ src/Api/Core/BlazorSozluk.Api.Domain/Models/EntryCommentVote.cs
@@ -0,0 +1,19 @@
+﻿using BlazorSozluk.Common.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Domain.Models
+{
+    public class EntryCommentVote:BaseEntity
+    {
+        public VoteType VoteType { get; set; }
+
+        public Guid EntryCommentId { get; set; }
+        public Guid CreatedById { get; set; }
+
+        public virtual EntryComment EntryComment { get; set; }
+    }
+}
---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/EntityConfiguration/Entry/EntryFavoriteEntityConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using BlazorSozluk.Api.Domain.Models;
+using BlazorSozluk.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistence.EntityConfiguration.Entry
+{
+    public class EntryFavoriteEntityConfiguration : BaseEntityConfiguration<Api.Domain.Models.EntryFavorite>
+    {
+        public override void Configure(EntityTypeBuilder<EntryFavorite> builder)
+        {
+            base.Configure(builder);
+
+            builder.ToTable("entryfavorite", BlazorSozlukContext.DEFAULT_SCHEMA);
+
+            builder.HasOne(i => i.Entry)
+                   .WithMany(i => i.EntryFavorites)
+                   .HasForeignKey(i => i.CreatedById);
+
+            builder.HasOne(i => i.CreatedUser)
+                  .WithMany(i => i.EntryFavorites)
+                  .HasForeignKey(i => i.CreatedById)
+                  .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
