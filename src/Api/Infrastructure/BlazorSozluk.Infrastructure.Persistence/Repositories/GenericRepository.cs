44818de0223d7fd99a7d657b74a94727aaad9f28
TARGE-HUSEYIN <TARGE-HUSEYIN@DESKTOP-L69KJ98>
2022-12-06 16:00:57 +0300

    dbcontext updated

--- src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Repositories/GenericRepository.cs
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Repositories/GenericRepository.cs
@@ -13,11 +13,11 @@ namespace BlazorSozluk.Infrastructure.Persistence.Repositories
 {
     public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
     {
-        private readonly BlazorSozlukContext dbContext;
+        private readonly DbContext dbContext;
         protected DbSet<TEntity> entity=>dbContext.Set<TEntity>();
 
 
-        public GenericRepository(BlazorSozlukContext dbContext)
+        public GenericRepository(DbContext dbContext)
         {
             this.dbContext= dbContext ?? throw new ArgumentNullException(nameof(dbContext));
         }
